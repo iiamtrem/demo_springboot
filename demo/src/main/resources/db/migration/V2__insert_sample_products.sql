b9387e6ee687ce97982d26a85ad186bacc0c856f
arabica_2 <tramtram03052004@gmail.com>
2025-07-25 11:07:12 +0700

    main

---
+++ demo/src/main/resources/db/migration/V2__insert_sample_products.sql
@@ -0,0 +1,7 @@
+INSERT INTO products (name, price, description)
+VALUES
+('Váy hoa', 499000, 'Váy chất liệu cotton thoáng mát'),
+('Áo sơ mi trắng', 299000, 'Áo sơ mi form basic'),
+('Quần jeans', 399000, 'Chất jean co giãn tốt'),
+('Đầm body', 550000, 'Chất vải thun lạnh'),
+('Chân váy', 350000, 'Dễ phối đồ');
